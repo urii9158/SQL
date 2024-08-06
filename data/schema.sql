535bbc4c1ee8f9badbe59a272fecdbfed6555f8a
uri <katyacitti@gmail.com>
2024-08-06 14:53:11 +0300

    Fixed

--- data/schema.sql
+++ data/schema.sql
@@ -1,26 +1,27 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
-    id       INT PRIMARY KEY AUTO_INCREMENT,
+    id       CHAR(36) PRIMARY KEY,
     login    VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255)        NOT NULL
+    password VARCHAR(255)        NOT NULL,
+    status   VARCHAR(255)        NOT NULL DEFAULT 'active'
 );
 
 DROP TABLE IF EXISTS cards;
 CREATE TABLE cards
 (
-    id                 INT PRIMARY KEY AUTO_INCREMENT,
-    user_id            INT NOT NULL,
+    id                 CHAR(36) PRIMARY KEY,
+    user_id            CHAR(36)           NOT NULL,
     number             VARCHAR(19) UNIQUE NOT NULL,
-    balance_in_kopecks INT                NOT NULL DEFAULT 0,
+    balance_in_kopecks INT                NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 DROP TABLE IF EXISTS auth_codes;
 CREATE TABLE auth_codes
 (
-    id      INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
+    id      CHAR(36) PRIMARY KEY,
+    user_id CHAR(36)   NOT NULL,
     code    VARCHAR(6) NOT NULL,
     created TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (id)
@@ -29,7 +30,7 @@ CREATE TABLE auth_codes
 DROP TABLE IF EXISTS card_transactions;
 CREATE TABLE card_transactions
 (
-    id                INT PRIMARY KEY AUTO_INCREMENT,
+    id                CHAR(36) PRIMARY KEY,
     source            VARCHAR(19) NOT NULL,
     target            VARCHAR(19) NOT NULL,
     amount_in_kopecks INT         NOT NULL,
